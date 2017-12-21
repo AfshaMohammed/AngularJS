1564d3cb8f41230de4e4bb9a92f3d694edfc7be1
AfshaMohammed <mdafsha.7@gmail.com>
2017-12-20 18:59:18 -0600

    AngularJS
    controller to controller communication

---
+++ AngularJS/ctrlToCtrl.js
@@ -0,0 +1,10 @@
+var app = angular.module("myApp", []);
+app.controller("firstCtrl", function($scope, myService){
+	$scope.user = myService.user;
+});
+app.controller("secondCtrl", function($scope, myService){
+	$scope.otheruser = myService.user;
+});
+app.service("myService", function(){
+	this.user = {};
+});
